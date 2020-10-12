b93b9e4d6f06fd44ac8b34a33684aa0168e777bd
Brian Holdman <bholdman@gmail.com>
2020-10-12 18:23:46 -0500

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "chargify", password: "qwerty123", password_confirmation: "qwerty123")
+Subscription.create([{
+  subscription_name: "Bronze Box",
+  subscription_price: 1900,
+  subscription_term: :month
+},
+{
+  subscription_name: "Silver Box",
+  subscription_price: 4900,
+  subscription_term: :month
+},
+{
+  subscription_name: "Gold Box",
+  subscription_price: 9900,
+  subscription_term: :month
+}])
